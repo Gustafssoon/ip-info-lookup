eb9fc41d9c8399f794ef0767802dbb4c5e5d2e26
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-06-15 11:17:50 +0200

    Lägger till kommentarer på main.ps1

--- main.ps1
+++ main.ps1
@@ -1,15 +1,21 @@
+# Tar emot IP-adressen som användaren vill slå upp.
 param (
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     [string]$IpAddress
 )
 
+# Importerar projektets egna moduler.
+# $PSScriptRoot gör att sökvägarna fungerar oavsett varifrån scriptet körs.
 Import-Module (Join-Path $PSScriptRoot "modules/IpInfo.Env.psm1") -Force
 Import-Module (Join-Path $PSScriptRoot "modules/IpInfo.Api.psm1") -Force
 Import-Module (Join-Path $PSScriptRoot "modules/IpInfo.Format.psm1") -Force
 
+# Läser in API-token från .env-filen.
 Import-IpInfoEnv -Path (Join-Path $PSScriptRoot ".env")
 
+# Hämtar information om IP-adressen från IPinfo API.
 $result = Get-IpInfo -IpAddress $IpAddress
 
+# Visar API-svaret i lättläst format.
 Show-IpInfoResult -IpInfo $result
