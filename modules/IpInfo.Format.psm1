e80d686a6051206c586f80a0ec8c80a15b5e511a
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-06-15 11:21:35 +0200

    Lägger till kommentarer för format.psm1

--- modules/IpInfo.Format.psm1
+++ modules/IpInfo.Format.psm1
@@ -1,9 +1,12 @@
+# Visar IPinfo-resultatet i ett enklare format i terminalen.
 function Show-IpInfoResult {
     param (
+        # Tar emot objektet som kommer tillbaka från Get-IpInfo.
         [Parameter(Mandatory = $true)]
         $IpInfo
     )
 
+    # Skriver ut valda delar av API-svaret.
     Write-Host "IP-information"
     Write-Host "--------------"
     Write-Host "IP-adress:  $($IpInfo.ip)"
@@ -11,4 +14,5 @@ function Show-IpInfoResult {
     Write-Host "Kontinent:  $($IpInfo.continent)"
 }
 
+# Gör funktionen tillgänglig när modulen importeras.
 Export-ModuleMember -Function Show-IpInfoResult
