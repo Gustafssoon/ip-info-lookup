788244c520c817f4f8e1d9b8a7f7a19f794cfc8a
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-06-15 11:00:59 +0200

    hämta IP-information från IPinfo API

--- modules/IpInfo.Api.psm1
+++ modules/IpInfo.Api.psm1
@@ -3,9 +3,19 @@ function Get-IpInfo {
         [string]$IpAddress
     )
 
+    if (-not $env:IPINFO_TOKEN) {
+        throw "IPINFO_TOKEN saknas. Kör Import-IpInfoEnv först."
+    }
+
     $uri = "https://api.ipinfo.io/lite/$IpAddress"
 
-    return $uri
+    $headers = @{
+        Authorization = "Bearer $env:IPINFO_TOKEN"
+    }
+
+    $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+
+    return $response
 }
 
 Export-ModuleMember -Function Get-IpInfo
